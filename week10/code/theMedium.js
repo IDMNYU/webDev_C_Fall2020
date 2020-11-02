b6f47c9a6a3aa8975c89dfd284baaf7524b6dfc6
rebleo <rml444@nyu.edu>
2020-11-01 20:57:57 -0500

    theMessage

---
+++ week10/code/theMedium.js
@@ -0,0 +1,12 @@
+console.log("the Message!!");
+
+// pass the HTML element into a variable
+// 
+let theButton = document.querySelector("button");
+
+
+// theButton.addEventListener('click', clickButton);
+
+// function clickButton(){
+//  console.log("cick!!!");
+// }
